657cc0aadd79fa02cef4032a122417d81b1d3e4f
Jeremias0618 <yeremitantaraico@gmail.com>
2026-04-16 18:25:28 -0500

    Ejercicio 1

---
+++ Semana 04/Ejercicio 1/C#/C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejercicio 1: Aplicar descuento por compra mínima.
+            // Si el monto es mayor o igual a 100, se aplica un 10% de descuento.
+            // En caso contrario, no se aplica ningún descuento.
+            // El total a pagar es el monto ingresado menos el descuento calculado.
+
+            // Ingresar el monto de la compra
+            Console.WriteLine("Ingrese el monto de la compra:");
+            int compra = int.Parse(Console.ReadLine());
+
+            // Inicializar con descuento 0
+
+            double descuento = 0;
+
+            // Verificar si se aplica el descuento
+
+            if (compra >= 100)
+            {
+                descuento = compra * 0.10; // Calcular el descuento del 10%
+            }
+
+            // calcular el total a pagar
+
+            double totalPagar = compra - descuento;
+
+            // Mostrar el resultado
+
+            Console.WriteLine($"Monto de la compra: {compra}");
+            Console.WriteLine($"Descuento aplicado: {descuento}");
+            Console.WriteLine($"Total a pagar: {totalPagar}");
+        }
+    }
+}
---
+++ Semana 04/Ejercicio 1/Python/ejercicio1.py
@@ -0,0 +1,4 @@
+# Ejercicio 1: Aplicar descuento por compra mínima.
+# Si el monto es mayor o igual a 100, se aplica un 10% de descuento.
+# En caso contrario, no se aplica ningún descuento.
+# El total a pagar es el monto ingresado menos el descuento calculado.
